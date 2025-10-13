4ea36211eb0a889b4931f26eb2846802b5dfd4dc
=anmolkumar-prive <=anmol.kumar@privetechnologies.com>
2025-10-13 16:10:13 +0530

    exp

---
+++ myapp/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Switching to main branch..."
+git checkout main
+
+echo "Deleting 'test1' branch (after merge)..."
+git branch -d test1
+
+echo "Recreating 'test1' branch from main..."
+git checkout -b test1
+
+echo "'test1' branch reset complete."
